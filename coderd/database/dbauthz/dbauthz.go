c58ff13df9feb44677e625f70d508cdf7b5f31c3
Danny Kopping <danny@coder.com>
2024-05-29 11:27:24 +0200

    Update SQL queries to handle bulk operations
    Signed-off-by: Danny Kopping <danny@coder.com>

--- coderd/database/dbauthz/dbauthz.go
+++ coderd/database/dbauthz/dbauthz.go
@@ -822,6 +822,18 @@ func (q *querier) BatchUpdateWorkspaceLastUsedAt(ctx context.Context, arg databa
 	return q.db.BatchUpdateWorkspaceLastUsedAt(ctx, arg)
 }
 
+func (q *querier) BulkMarkNotificationMessageFailed(ctx context.Context, arg database.BulkMarkNotificationMessageFailedParams) (int64, error) {
+	panic("not implemented")
+}
+
+func (q *querier) BulkMarkNotificationMessagesInhibited(ctx context.Context, arg database.BulkMarkNotificationMessagesInhibitedParams) (int64, error) {
+	panic("not implemented")
+}
+
+func (q *querier) BulkMarkNotificationMessagesSent(ctx context.Context, arg database.BulkMarkNotificationMessagesSentParams) (int64, error) {
+	panic("not implemented")
+}
+
 func (q *querier) CleanTailnetCoordinators(ctx context.Context) error {
 	if err := q.authorizeContext(ctx, policy.ActionDelete, rbac.ResourceTailnetCoordinator); err != nil {
 		return err
@@ -971,7 +983,7 @@ func (q *querier) DeleteOAuth2ProviderAppTokensByAppAndUserID(ctx context.Contex
 	return q.db.DeleteOAuth2ProviderAppTokensByAppAndUserID(ctx, arg)
 }
 
-func (q *querier) DeleteOldNotificationMessages(ctx context.Context) error {
+func (q *querier) DeleteOldNotificationMessages(ctx context.Context, maxAttemptCount int32) error {
 	panic("not implemented")
 }
 
@@ -2743,18 +2755,6 @@ func (q *querier) ListWorkspaceAgentPortShares(ctx context.Context, workspaceID 
 	}
 
 	return q.db.ListWorkspaceAgentPortShares(ctx, workspaceID)
-}
-
-func (q *querier) MarkNotificationMessageFailed(ctx context.Context, arg database.MarkNotificationMessageFailedParams) (database.NotificationMessage, error) {
-	panic("not implemented")
-}
-
-func (q *querier) MarkNotificationMessageSent(ctx context.Context, arg database.MarkNotificationMessageSentParams) (database.NotificationMessage, error) {
-	panic("not implemented")
-}
-
-func (q *querier) MarkNotificationMessagesInhibited(ctx context.Context, arg database.MarkNotificationMessagesInhibitedParams) (database.NotificationMessage, error) {
-	panic("not implemented")
 }
 
 func (q *querier) ReduceWorkspaceAgentShareLevelToAuthenticatedByTemplate(ctx context.Context, templateID uuid.UUID) error {
